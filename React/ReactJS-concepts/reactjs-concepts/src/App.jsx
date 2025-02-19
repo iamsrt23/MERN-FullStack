3879d253aff65499b75fe0ba720127db5dd98b2c
iamsrt23 <ravi.teja.work95@gmail.com>
2025-02-19 13:16:06 +0530

    React-JS-Statemanagement-basics-added

--- React/ReactJS-concepts/reactjs-concepts/src/App.jsx
+++ React/ReactJS-concepts/reactjs-concepts/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import ClassBasedComponent from './components/class-based-component'
 import ProductList from './components/products'
 
 const dummyProductData = ['Product 1','Product 2','Product 3']
@@ -10,9 +11,9 @@ function App() {
   return (
     <>
       <h1>React JS Concepts 2025</h1>
-      {/* <ClassBasedComponent />
-      <FunctionalComponent /> */}
-      <ProductList listOfProducts= {dummyProductData} name='Teja' city="HYD"/>
+      <ClassBasedComponent />
+      {/* <FunctionalComponent /> */} 
+      {/* <ProductList listOfProducts= {dummyProductData} name='Teja' city="HYD"/> */}
     </>
   )
 }
